d69e68060dc570d35157682bfacf35bf475d0cf2
pruesinghnagra <pruesinghnagra@gmail.com>
2020-11-20 11:40:18 +1300

    Amended path to file

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from '../../the-one-boilerplate/client/components/App'
+import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
