ca83939426c8cacdbad02c0ef15de9ab159ce58b
pruesinghnagra <pruesinghnagra@gmail.com>
2020-11-20 11:28:01 +1300

    Added React components

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from '../../the-one-boilerplate/client/components/App'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+  )
+})
